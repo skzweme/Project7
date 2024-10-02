255671ce7adf0c36cccaf3ada5560b3b51f7ed0c
skzweme <kateshaffr@gmail.com>
2024-10-01 21:21:44 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -266,7 +266,7 @@ function populateRankingEntry(trainee, currRank) {
     </div>
     <div class="ranking__row-text">
       <div class="name"><strong>${trainee.name_romanized}</strong></div>
-      // <div class="company">${modifiedCompany}</div>
+      <div class="company">${modifiedCompany}</div>
     </div>
   </div>`;
   return rankingEntry;
